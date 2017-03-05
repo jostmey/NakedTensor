8d5a5ed224f76a3b1d3180d2ee650e5e00a51079
mynameisvinn <vin.tang@gmail.com>
2017-03-05 09:15:11 -0500

    fix cost function

--- tensor.py
+++ tensor.py
@@ -12,7 +12,7 @@ m = tf.Variable(m_initial) # Parameters
 b = tf.Variable(b_initial)
 
 ys_model = m*xs+b # Tensorflow knows this is a vector operation
-error = tf.reduce_sum((ys-ys_model)**2) # Sum up every item in the vector
+error = tf.reduce_sum((ys-ys_model)**2)/(2*len(xs)) # Sum up every item in the vector
 
 operation = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(error) # Does one step
 
