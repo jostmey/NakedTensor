43cb7c2a87318d373b6faf873d63ebadc5ce1ecb
Jared Ostmeyer <jostmey@gmail.com>
2017-03-12 20:54:54 -0500

    Made serial.py and tensor.py consistent

--- serial.py
+++ serial.py
@@ -6,39 +6,45 @@ xs = [ 0.00,  1.00,  2.00, 3.00, 4.00, 5.00, 6.00, 7.00] # Features
 ys = [-0.82, -0.94, -0.12, 0.26, 0.39, 0.64, 1.02, 1.00] # Labels
 
 '''
-with enough iterations, initial weights dont matter since our cost function is convex.
+With enough iterations, initial weights dont matter since our cost function is convex.
 '''
 m_initial = -0.5 # Initial guesses
 b_initial =  1.0
 
 
 '''
-define free variables to be solved. we'll be taking partial derivatives of m and b with respect to j (cost).
+Define free variables to be solved. We will be taking partial derivatives of m and b with respect to j (cost).
 '''
 m = tf.Variable(m_initial) # Parameters
 b = tf.Variable(b_initial)
 
-error = 0.0
+'''
+Define the error between the data and the model one point at a time (slow).
+'''
+total_error = 0.0
 for i in range(len(xs)):
 	y_model = m*xs[i]+b # Output of the model aka yhat
-	error += (ys[i]-y_model)**2 # Difference squared - this is the "cost" to be minimized
-
+	total_error += (ys[i]-y_model)**2 # Difference squared - this is the "cost" to be minimized
 
 '''
-once cost function is defined, use gradient descent to find global minimum.
+Once cost function is defined, create gradient descent optimizer.
 '''
+optimizer_operation = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(total_error) # Does one step
 
-operation = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(error) # Does one step
+'''
+Something you just have to do.
+'''
+initializer_operation = tf.global_variables_initializer()
 
-
-
+'''
+Train model.
+'''
 with tf.Session() as session:
-	session.run(tf.initialize_all_variables()) # Initialize session
+
+	session.run(initializer_operation)
 
 	_EPOCHS = 10000 # number of "sweeps" across data
-
 	for iteration in range(_EPOCHS):
-		session.run(operation)
+		session.run(optimizer_operation)
 
 	print('Slope:', m.eval(), 'Intercept:', b.eval())
-
--- tensor.py
+++ tensor.py
@@ -5,22 +5,44 @@ import tensorflow as tf
 xs = [ 0.00,  1.00,  2.00, 3.00, 4.00, 5.00, 6.00, 7.00] # Features
 ys = [-0.82, -0.94, -0.12, 0.26, 0.39, 0.64, 1.02, 1.00] # Labels
 
+'''
+With enough iterations, initial weights dont matter since our cost function is convex.
+'''
 m_initial = -0.5 # Initial guesses
 b_initial =  1.0
 
+'''
+Define free variables to be solved. We will be taking partial derivatives of m and b with respect to j (cost).
+'''
 m = tf.Variable(m_initial) # Parameters
 b = tf.Variable(b_initial)
 
+'''
+Define the error between the data and the model as a tensor (distributed computing)
+'''
 ys_model = m*xs+b # Tensorflow knows this is a vector operation
-error = tf.reduce_sum((ys-ys_model)**2)/(2*len(xs)) # Sum up every item in the vector
+total_error = tf.reduce_sum((ys-ys_model)**2) # Sum up every item in the vector
 
-operation = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(error) # Does one step
+'''
+Once cost function is defined, create gradient descent optimizer.
+'''
+optimizer_operation = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(total_error) # Does one step
 
+'''
+Something you just have to do.
+'''
+initializer_operation = tf.global_variables_initializer()
+
+'''
+Train model.
+'''
 with tf.Session() as session:
-	session.run(tf.initialize_all_variables()) # Initialize session
 
-	for iteration in range(10000):
-		session.run(operation)
+	session.run(initializer_operation)
+
+	_EPOCHS = 10000 # number of "sweeps" across data
+	for iteration in range(_EPOCHS):
+		session.run(optimizer_operation)
 
 	print('Slope:', m.eval(), 'Intercept:', b.eval())
 
