80b21a1011fde609ce880f2f8881e0e8be3de7b8
Jared Ostmeyer <jostmey@gmail.com>
2017-03-12 22:00:45 -0500

    Update serial.py

--- serial.py
+++ serial.py
@@ -36,7 +36,7 @@ Something you just have to do.
 initializer_operation = tf.global_variables_initializer()
 
 '''
-Train model.
+All calculations are done in a session.
 '''
 with tf.Session() as session:
 
